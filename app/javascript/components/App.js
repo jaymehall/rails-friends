f0dacd548aa166ffd0d7b1c5ecb8cbefd5b88566
Jayme <jlm_developer@protonmail.com>
2021-07-03 13:02:40 -0700

    styled friends page

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 // import Form from './reusable/Form/Form';
 
 function App() {
-    const text = "React App is here!"
-    return (
-        <>
-            <div>{text}</div>
-            {/* <Form buttonText = "I'm a Prop"/> */}
-        </>
-    )
+  const text = "React App is here!";
+  return (
+    <>
+      <div>{text}</div>
+      {/* <Form buttonText = "I'm a Prop"/> */}
+    </>
+  );
 }
 
 export default App;
