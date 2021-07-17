707659d04787fbdbbfb036e8f56915305b70a0fb
Jayme <jlm_developer@protonmail.com>
2021-07-16 19:59:10 -0700

    styled homepage with React App Component and react-bootstrap with background and CTA button

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-// import Form from './reusable/Form/Form';
+import "./App.css";
+import { Button } from 'react-bootstrap';
 
 function App() {
-  const text = "React App is here!";
   return (
-    <>
-      <div>{text}</div>
-      {/* <Form buttonText = "I'm a Prop"/> */}
-    </>
+    <div className="title">
+      <h2>Keep Track of your Friends</h2>
+      <a rel="nofollow" href="/friends/new"><Button className="btn btn-dark">Get Started</Button></a>
+    </div>
   );
 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :friends
-  #get 'home/index'
   get 'home/about'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
